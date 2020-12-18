4bfd55aaec19cbe1bb055db283ba81c301a632d9
Mrxxm <362190221@qq.com>
2020-12-18 14:59:00 +0800

    AppId service 列表方法提交

--- app/Services/Impl/AppIdServiceImpl.php
+++ app/Services/Impl/AppIdServiceImpl.php
@@ -4,6 +4,7 @@
 namespace App\Services\Impl;
 
 
+use App\Models\AppIdModel;
 use App\Services\AppIdService;
 
 class AppIdServiceImpl implements AppIdService
@@ -17,12 +18,29 @@ class AppIdServiceImpl implements AppIdService
 
     public function list(array $data): array
     {
-        // TODO: Implement list() method.
+        $select = ['id', 'app_name', 'app_key', 'app_expire', 'stitching_symbol', 'description', 'create_time', 'update_time'];
+
+        $keyword = $data['keyword'] ?? '';
+        $conditions = [];
+        $conditions[] = ['status', '=', 1];
+        if (!empty($keyword)) {
+            $conditions[] = ['app_name', 'like', "%{$keyword}%"];
+        }
+
+        $result = $this->model->list($select, $conditions);
+
+        if (count($result)) {
+            foreach ($result['data'] as &$res) {
+                $res['delete_date'] = DateFormat($res['delete_time']);
+            }
+        }
+
+        return $result;
     }
 
     public function add(array $fields): void
     {
-        // TODO: Implement add() method.
+        $this->model->add($fields);
     }
 
     public function update(int $id, array $fields): void
