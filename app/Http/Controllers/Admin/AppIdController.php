9ac167037b9bcf2666a6dbb2606bcd6e983f0e58
Mrxxm <362190221@qq.com>
2020-12-18 14:53:25 +0800

    AppIdController 添加action提交

--- app/Http/Controllers/Admin/AppIdController.php
+++ app/Http/Controllers/Admin/AppIdController.php
@@ -32,4 +32,31 @@ class AppIdController
 
         return Response::successResponse($result);
     }
+
+    public function add(Request $request)
+    {
+        $data = $request->only('app_name', 'app_key', 'app_expire', 'stitching_symbol', 'description');
+
+        $validator = Validator::make($data, [
+            'app_name'             => 'required|string',
+            'app_key'              => 'required|string',
+            'app_expire'           => 'required|integer',
+            'stitching_symbol'     => 'required|string',
+            'description'          => 'string',
+        ]);
+
+        if ($validator->fails()) {
+            return Response::missParamResponse();
+        }
+
+        $service = new AppIdServiceImpl();
+
+        try {
+             $service->add($data);
+        } catch (\Exception $exception) {
+            return Response::errorResponse([], $exception->getMessage());
+        }
+
+        return Response::successResponse();
+    }
 }
