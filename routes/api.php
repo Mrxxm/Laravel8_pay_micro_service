7888169aef589c1df8739e12caa96aed1fa7d3cb
Mrxxm <362190221@qq.com>
2020-12-18 17:01:24 +0800

    api.php统一下单路由创建

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['namespace' => 'App\Http\Controllers\Api'], function () {
+
+    Route::group(['middleware' => ['checkPayToken']], function () {
+
+        Route::any('pay/unifiedOrder', 'PayController@unifiedOrder');
+
+    });
+});
