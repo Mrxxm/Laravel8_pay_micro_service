5a9e99159dd7caf68e0c2d9b30a4656c9182ebdf
Mrxxm <362190221@qq.com>
2020-12-18 17:17:19 +0800

    后台路由注释

--- routes/admin.php
+++ routes/admin.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'App\Http\Controllers\Admin'], function () {
 
     // appId管理
-    Route::get('/appId/list','AppIdController@list');
+//    Route::get('/appId/list','AppIdController@list');
     Route::post('/appId/add','AppIdController@add');
-    Route::post('/appId/update','AppIdController@update');
-    Route::post('/appId/delete','AppIdController@delete');
+
 });
