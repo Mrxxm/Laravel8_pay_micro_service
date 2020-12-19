5464a2d6fe8c0661d53cb022bb5919366435e1f5
Mrxxm <362190221@qq.com>
2020-12-19 13:57:12 +0800

    ClassArr提交

---
+++ app/Utils/ClassArr.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class ClassArr
+{
+    public static function importClassStat()
+    {
+        return [
+            // pay
+            "ali_pay"            => "",
+            "weixin_pay"         => "",
+            // ocr
+            "ali_ocr"            => "App\Utils\OCR\AliOcr",
+            // sms
+            "ali_sms"            => "App\Utils\SMS\AliSms",
+            "mw_sms"             => "App\Utils\SMS\MWSms",
+            // oss
+            "ali_oss"            => "App\Utils\OSS\AliOss",
+            "ali_oss_new"        => "App\Utils\OSS\AliOssNew",
+            "qi_niu_oss"         => "App\Utils\OSS\QiNiuOss",
+        ];
+    }
+
+    public static function initClass($type, $classs, $params = [], $needInstance = false)
+    {
+        if(!array_key_exists($type, $classs)) {
+            throw new \Exception("类型：{$type} 的类库找不到");
+        }
+        $className = $classs[$type];
+
+        return $needInstance == true ? (new \ReflectionClass($className))->newInstanceArgs($params) : $className;
+
+    }
+}
