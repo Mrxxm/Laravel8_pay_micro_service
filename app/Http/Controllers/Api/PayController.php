7be2f9f3c2fea4c2d71426042ec628aca8d495f5
Mrxxm <362190221@qq.com>
2020-12-19 14:22:29 +0800

    支付控制器

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Http\Controllers\Response;
+use App\Services\Impl\PayServiceImpl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -12,21 +13,24 @@ class PayController
 {
     public function unifiedOrder(Request $request)
     {
-        return Response::successResponse();
-        $data = $request->only('keyword');
+        $data = $request->only('serve_type', 'pay_type', 'body', 'order_no', 'total_price');
 
         $validator = Validator::make($data, [
-            'keyword'             => 'string',
+            'serve_type'       => 'required|string',
+            'pay_type'         => 'required|string',
+            'body'             => 'required|string',
+            'order_no'         => 'required|string',
+            'total_price'      => 'required|string',
         ]);
 
         if ($validator->fails()) {
             return Response::missParamResponse([], $validator->errors()->first());
         }
 
-        $service = new AppIdServiceImpl();
+        $service = new PayServiceImpl();
 
         try {
-            $result = $service->list($data);
+            $result = $service->unifiedOrder($data);
         } catch (\Exception $exception) {
             return Response::errorResponse([], $exception->getMessage());
         }
