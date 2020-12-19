f8c4f33b2712e25cf0159e1ab941af02f5323945
Mrxxm <362190221@qq.com>
2020-12-19 14:46:42 +0800

    路由改为post提交

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'App\Http\Controllers\Api'], function () {
 
+    Route::post('pay/wechatNotify', 'PayController@wechatNotify');
+
     Route::group(['middleware' => ['checkPayToken']], function () {
 
         Route::post('pay/unifiedOrder', 'PayController@unifiedOrder');
