6b564a4f74c155a33cc6c4029fb59640a7bd6d00
Mrxxm <362190221@qq.com>
2021-01-07 20:42:38 +0800

    redis添加端口

--- app/Utils/Redis.php
+++ app/Utils/Redis.php
@@ -12,7 +12,7 @@ class Redis
     private function __construct()
     {
         $this->redis =  new \Redis();
-        $this->redis->connect('127.0.0.1');
+        $this->redis->connect('127.0.0.1', '6379');
         $this->redis->select(9);
     }
     public static function getInstance()
